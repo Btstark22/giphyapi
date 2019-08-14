99a2183c920cbda86fb99f237789a8ac79b715eb
Brandon Stark <btstark22@gmail.com>
2019-08-14 14:43:46 -0500

    changes for John

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let topics = ["disney"];
+let topics = [];
 const apikey = "NA7vL6oTxh01xHHphhmZIVNjvEOo8gWG";
 const newBtn = document.getElementById("new-btn");
 
@@ -12,29 +12,29 @@ function click() {
     //add a push statement that puts the users input into the topics array and makes a button
     for (let i = 0; i < topics.length; i++) {
         const btnEl = document.createElement("button");
-        btnEl.classList.add("btn");
+        btnEl.classList.add("old-btn");
         //btnEl.getAttribute.add(topics[i].trim().toLowerCase()) google search for this^ to 
         btnEl.innerHTML = topics[i];
         btnDiv.appendChild(btnEl);
         console.log(btnEl);
+        document.getElementsByClassName(".old-btn")(function(node){
+            node.addEventListener("click", make(topics[i]))
+        })
        
     }
-   let buttons = document.getElementsByClassName("btn")
-   for (let i = 0; buttons.length; i++) {
-       console.log(i)
-       console.log(buttons)
-       buttons[i].addEventListener("click", make);
+
+      
    }
-}
-function make() {
-    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NA7vL6oTxh01xHHphhmZIVNjvEOo8gWG&q=" + topics.value + "&limit=10&offset=0&rating=PG-13&lang=en";
+function make(titles) {
+    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NA7vL6oTxh01xHHphhmZIVNjvEOo8gWG&q=" + titles + "&limit=10&offset=0&rating=PG-13&lang=en";
+    console.log(titles)
         fetch(queryURL).then(function (response) {
             return response.json()
         })
             .then(function (responseJson) {
                 console.log(responseJson);
                 for ( let i = 0; i < responseJson.data.length; i++) {
-                const newImage = document.createElement("img");
+                let newImage = document.createElement("img");
                 newImage.setAttribute("src", responseJson.data[i].images.fixed_height.url)
                 document.getElementById("giph-div").appendChild(newImage)
                 }

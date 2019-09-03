a15b8c6c14d2fb9469e253bd6bb12cdd66ce5eff
Brandon Stark <btstark22@gmail.com>
2019-09-02 22:10:48 -0500

    finished Giphy-API with Jeremy

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function click() {
         btnEl.innerHTML = topics[i];
         btnDiv.appendChild(btnEl);
         console.log(btnEl);
-            btnEl.addEventListener("click", function(){
+        btnEl.addEventListener("click", function () {
+            document.getElementById("giph-div").innerHTML = "";
             const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NA7vL6oTxh01xHHphhmZIVNjvEOo8gWG&q=" + topics[i] + "&limit=10&offset=0&rating=PG-13&lang=en";
             fetch(queryURL).then(function (response) {
                 return response.json()
@@ -27,10 +28,25 @@ function click() {
                     for (let i = 0; i < responseJson.data.length; i++) {
                         let newImage = document.createElement("img");
                         newImage.setAttribute("src", responseJson.data[i].images.fixed_height.url)
-                        document.getElementById("giph-div").prepend(newImage)
+                        newImage.setAttribute("class", "gif")
+                        newImage.setAttribute("data-alt", responseJson.data[i].images.fixed_height_still.url);
+                        const newImageDiv = document.createElement("div");
+                        newImageDiv.append(newImage);
+                        const rating = document.createElement("h3");
+                        rating.textContent = "Rating: " + responseJson.data[i].rating;
+                        newImageDiv.append(rating);
+                        newImageDiv.className = "col-md-4";
+                        document.getElementById("giph-div").prepend(newImageDiv);
                     }
+                    document.querySelectorAll(".gif").forEach(function (img) {
+                        img.addEventListener("click", function (e) {
+                        const hodor = this.getAttribute("src");
+                        this.setAttribute("src", this.getAttribute("data-alt"))
+                        this.setAttribute("data-alt", hodor);
+                        })
+                    })
                 });
-            })
+        })
 
     }
 }
